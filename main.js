bdf2df25082889e90bc88d5080577b5404423b38
IAmInept <gianwallace2002@gmail.com>
2024-02-08 00:27:26 +0000

    Updated Price Functionality and cleaned code foramtting.

--- main.js
+++ main.js
@@ -6,31 +6,33 @@ const drinkSize = document.getElementById("bevSize");
 const smoothieBase = document.getElementById("smoothieBase");
 const milkshakeBase = document.getElementById("milkshakeBase");
 const milkshakeBaseExtra = document.getElementById("milkshakeExtra");
-const drinkType = document.getElementById("drinkType")
-const resetPage = document.getElementById("resetPage")
-const saveFav = document.getElementById("saveFavourite")
-const orderFave = document.getElementById("orderFavourite")
-const txtCost = document.getElementById("cost")
+const drinkType = document.getElementById("drinkType");
+const resetPage = document.getElementById("resetPage");
+const saveFav = document.getElementById("saveFavourite");
+const orderFave = document.getElementById("orderFavourite");
+const milkshakeExtra = document.getElementsByName("milkshakeExtraItem");
+const txtCost = document.getElementById("cost");
 
 //establishing variable costs for cost processing and display.
 let orderSize;
-let sizeS;
-let sizeM;
-let sizeL;
-let sizeXL;
+let orderCost;
 let sizeCost;
 let milkshakeExtraCost;
 
 //Adding change event listener and calling function to check what is checked and apply appropriate feature.
+drinkSize.addEventListener("change", checkSizeCost)
 drinkType.addEventListener("change", drinkTypeChange)
 resetPage.addEventListener("click", initialiseStartup)
 saveFav.addEventListener("click", saveFavourite)
 orderFave.addEventListener("click", orderFavourite)
-drinkSize.addEventListener("change", checkSizeCost)
+milkshakeExtra.forEach(item => item.addEventListener("change", checkMilkshakeExtra))
+
 
 //Calling start function
 initialiseStartup()
 
+
+// ---------------------------------------------------------
 
 function initialiseStartup() {
     console.log("Startup initialised")
@@ -40,16 +42,10 @@ function initialiseStartup() {
     console.log("Startup configured.")
 }
 function setVariables() {
-    //setting drink size prices
-    sizeS = 2.70;
-    sizeM = 3.20;
-    sizeL = 3.70;
-    sizeXL = 4.50;
-    // setting milkshake extra costs
-    milkshakeExtraCost = 0.85;
-    //setting default size cost to medium
+    milkshakeExtraCost = 0;
     sizeCost = 3.20;
-    txtCost.innerText = `${"£" + sizeCost.toFixed(2)}`
+    orderCost = sizeCost + milkshakeExtraCost
+    txtCost.innerText = `${"£" + orderCost.toFixed(2)}`
     //setting default drink and smoothieBase to checked
 }
 function uncheckItems() {
@@ -72,6 +68,15 @@ function uncheckItems() {
     document.getElementById("smoothieBase_orangeJuice").checked = true
     document.getElementById("milkshakeBase_skimmedMilk").checked = true
 }
+function hideExtras() {
+    //hiding additional fieldset that are not needed to be shown
+    smoothieBase.classList.add("hidden");
+    milkshakeBase.classList.add("hidden");
+    milkshakeBaseExtra.classList.add("hidden");
+}
+
+// ---------------------------------------------------------
+
 function drinkTypeChange() {
     if (document.getElementById("drinkType_smoothie").checked) { // checks if smoothie radio button is checked.
         console.log("Smoothie has been selected, Displaying relevant fields."); // logs to confirm if statement is functioned
@@ -84,12 +89,6 @@ function drinkTypeChange() {
         milkshakeBaseExtra.classList.remove("hidden");
         smoothieBase.classList.add("hidden");
     }
-}
-function hideExtras() {
-    //hiding additional fieldset that are not needed to be shown
-    smoothieBase.classList.add("hidden");
-    milkshakeBase.classList.add("hidden");
-    milkshakeBaseExtra.classList.add("hidden");
 }
 function checkSizeCost() {
  if (document.getElementById("sizeS").checked) {
@@ -105,8 +104,44 @@ function checkSizeCost() {
      sizeCost = 4.50;
      orderSize = "Extra Large";
  }
- txtCost.innerText = `${"£" + sizeCost.toFixed(2)}`
+    orderCost = sizeCost + milkshakeExtraCost
+ txtCost.innerText = `${"£" + orderCost.toFixed(2)}`
 }
+
+// ---------------------------------------------------------
+
+function checkMilkshakeExtra() {
+    if (this.value == "malt") {
+        if (this.checked) {
+            milkshakeExtraCost += 0.85;
+        } else {
+            milkshakeExtraCost -= 0.85;
+        }
+    } else if (this.value == "marshmallows") {
+        if (this.checked) {
+            milkshakeExtraCost += 0.85;
+        } else {
+            milkshakeExtraCost -= 0.85;
+        }
+    } else if (this.value == "whippedCream") {
+        if (this.checked) {
+            milkshakeExtraCost += 0.85;
+        } else {
+            milkshakeExtraCost -= 0.85;
+        }
+    } else if (this.value == "flake") {
+        if (this.checked) {
+            milkshakeExtraCost += 0.85;
+        } else {
+            milkshakeExtraCost -= 0.85;
+        }
+    }
+    orderCost = sizeCost + milkshakeExtraCost
+    txtCost.innerText = `${"£" + orderCost.toFixed(2)}`
+}
+
+// ---------------------------------------------------------
+
 function saveFavourite() {
     // write to json file
     console.log("Order Saved");
